db9034be55df0ef4f183a62035a5ac94d41c4cd2
sox <gitlab@simplel.de>
2019-09-20 17:48:56 +0200

    Add a reminder for the one who takes over

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,3 +1,9 @@
 <?php
 
- $GLOBALS['TL_CRON']['minutely'][] = array('SeptemberWerbeagentur\ContaoAtomNewsIntegrationBundle\AtomFeedImporter', 'updateFeeds');
+$GLOBALS['TL_CRON']['monthly'][] = array('SeptemberWerbeagentur\ContaoAtomNewsIntegrationBundle\AtomFeedImporter', 'updateFeeds');
+
+/* TODO use backend selection to customize frequency */
+//$GLOBALS['TL_CRON']['minutely'][] = array('SeptemberWerbeagentur\ContaoAtomNewsIntegrationBundle\AtomFeedImporter', 'updateFeedsMinutely');
+//$GLOBALS['TL_CRON']['hourly'][] = array('SeptemberWerbeagentur\ContaoAtomNewsIntegrationBundle\AtomFeedImporter', 'updateFeedsHourly');
+//$GLOBALS['TL_CRON']['daily'][] = array('SeptemberWerbeagentur\ContaoAtomNewsIntegrationBundle\AtomFeedImporter', 'updateFeedsDaily');
+//$GLOBALS['TL_CRON']['monthly'][] = array('SeptemberWerbeagentur\ContaoAtomNewsIntegrationBundle\AtomFeedImporter', 'updateFeedsMonthly');
