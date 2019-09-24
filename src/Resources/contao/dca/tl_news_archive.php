a4677377a3bb79e3e5e6992be8233f82e65872e1
Carl Richter <richter@september-werbeagentur.de>
2019-09-24 12:23:39 +0200

    remove update interval option temporarily

--- src/Resources/contao/dca/tl_news_archive.php
+++ src/Resources/contao/dca/tl_news_archive.php
@@ -5,7 +5,7 @@ $GLOBALS['TL_DCA']['tl_news_archive']['palettes']['__selector__'][] = 'september
 $GLOBALS['TL_DCA']['tl_news_archive']['palettes']['default'] = str_replace('jumpTo;',
     'jumpTo;{september_atom_legend},septemberUseAtom;',
     $GLOBALS['TL_DCA']['tl_news_archive']['palettes']['default']);
-$GLOBALS['TL_DCA']['tl_news_archive']['subpalettes']['septemberUseAtom'] = 'september_atom_url,september_atom_update_interval';
+$GLOBALS['TL_DCA']['tl_news_archive']['subpalettes']['septemberUseAtom'] = 'september_atom_url';
 
 $GLOBALS['TL_DCA']['tl_news_archive']['fields']['septemberUseAtom'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_news_archive']['september_atom'],
@@ -21,17 +21,4 @@ $GLOBALS['TL_DCA']['tl_news_archive']['fields']['september_atom_url'] = [
     'inputType' => 'text',
     'eval' => ['rgxp' => 'url', 'mandatory' => true, 'tl_class' => 'w50 clr'],
     'sql' => "varchar(255) NOT NULL default ''"
-];
-
-$GLOBALS['TL_DCA']['tl_news_archive']['fields']['september_atom_update_interval'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_news_archive']['september_atom_update_interval'],
-    'exclude' => true,
-    'inputType' => 'select',
-    'options' => [
-        'monthly' => &$GLOBALS['TL_LANG']['tl_news_archive']['september_atom_monthly'],
-        'daily' => &$GLOBALS['TL_LANG']['tl_news_archive']['september_atom_daily'],
-        'hourly' => &$GLOBALS['TL_LANG']['tl_news_archive']['september_atom_hourly'],
-    ],
-    'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
-    'sql' => "varchar(20) NOT NULL default ''"
 ];
